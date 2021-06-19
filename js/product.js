64592e8a525695aee22bb58c8ba6cfa7f012814f
mikaelrev <mikael.myrvang@gmail.com>
2021-06-19 12:55:53 +0200

    added js

--- js/product.js
+++ js/product.js
@@ -3,3 +3,21 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+const productContainer = document.querySelector(".container");
+
+let displayPrice = "Unavailable";
+
+if(product.price) {
+    displayPrice = product.price;
+}
+
+let decoration = "none";
+
+if(product.onSale) {
+    decoration = "underline";
+}
+productContainer.innerHTML = `<div class="product">
+                       <h4 style="text-decoration: ${decoration}">${product.name}</h4>
+                       <div>Price: ${displayPrice}</div>     
+                    </div>`;
